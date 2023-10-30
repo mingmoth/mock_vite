09fd58e5bfe449164cf0933b8e2e0a33763c4aaf
mingmoth <mingmoth@gmail.com>
2023-10-30 17:14:56 +0800

    feat: add try catch in indexHTMLMiddleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,14 +1,21 @@
 import { getFilePathAndContentType } from "./utils.js"
 
 const indexHTMLMiddleware = async (req, res) => {
-    const { filePath, contentType } = getFilePathAndContentType(req.url)
+    try {
+        const { filePath, contentType } = getFilePathAndContentType(req.url)
 
-    const file = Bun.file(filePath)
-    const content = await file.text()
-    res.writeHead(200, {
-        "Content-Type": contentType
-    })
-    res.end(content)
+        const file = Bun.file(filePath)
+        const content = await file.text()
+        res.writeHead(200, {
+            "Content-Type": contentType
+        })
+        res.end(content)
+    } catch (error) {
+        res.writeHead(500, {
+            "Content-Type": "text/plain"
+        })
+        res.end("You have to create an index.html")
+    }
 }
 
 export {
