69b86424777b1a9d1ad1321aec80a7a9a91f0f3c
mingmoth <mingmoth@gmail.com>
2023-10-30 16:36:03 +0800

    refactor: extract index create server process into server.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import http from "http"
-import color from "picocolors"
-import connect from "connect"
-import { indexHTMLMiddleware } from "./src/middlewares"
+import { createServer } from "./src/server"
 
-const { PORT_HTTP, PROJECT_NAME } = process.env
-
-const middleware = connect()
-
-
-middleware.use(indexHTMLMiddleware)
-
-http
-  .createServer(middleware)
-  .listen(PORT_HTTP, () => {})
-
-console.log(color.green(`${PROJECT_NAME}`), 'server started at' ,color.green(`http://localhost:${PORT_HTTP}`))
+createServer();
