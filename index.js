afabe1b5eee41bf17c47590269fb6be6e1cbf039
mingmoth <mingmoth@gmail.com>
2023-10-30 16:21:55 +0800

    feat: add connect middleware in index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import http from "http"
 import color from "picocolors"
+import connect from "connect"
 
 const { PORT_HTTP, PROJECT_NAME } = process.env
 
+const middleware = connect()
+const indexHTMLMiddleware = (req, res) => {
+    res.end("hello from connect middleware")
+}
+
+middleware.use(indexHTMLMiddleware)
+
 http
-  .createServer((req, res) => {
-    res.end("hello world")
-  })
+  .createServer(middleware)
   .listen(PORT_HTTP, () => {})
 
 console.log(color.green(`${PROJECT_NAME}`), 'server started at' ,color.green(`http://localhost:${PORT_HTTP}`))
