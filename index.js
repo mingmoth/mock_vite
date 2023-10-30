edd7ed16146a3799db47b0e9cf35f35a9b9b3c92
mingmoth <mingmoth@gmail.com>
2023-10-30 16:15:10 +0800

    feat: add bun scripts & create http server with picocolors

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import http from "http"
+import color from "picocolors"
+
+const { PORT_HTTP, PROJECT_NAME } = process.env
+
+http
+  .createServer((req, res) => {
+    res.end("hello world")
+  })
+  .listen(PORT_HTTP, () => {})
+
+console.log(color.green(`${PROJECT_NAME}`), 'server started at' ,color.green(`http://localhost:${PORT_HTTP}`))
