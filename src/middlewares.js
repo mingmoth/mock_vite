1ea0b97d13b012205ed143d21d29dbf65fb9e525
mingmoth <mingmoth@gmail.com>
2023-10-30 17:58:37 +0800

    feat: add client.js & write script into req head

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,4 @@
+import path from "path"
 import { getFilePathAndContentType } from "./utils.js"
 
 const indexHTMLMiddleware = async (req, res) => {
@@ -5,7 +6,18 @@ const indexHTMLMiddleware = async (req, res) => {
         const { filePath, contentType } = getFilePathAndContentType(req.url)
 
         const file = Bun.file(filePath)
-        const content = await file.text()
+        let content = await file.text()
+
+        const clientScript = `<script src="src/client.js"></script>`
+
+        if(path.basename(filePath === 'index.html')) {
+            const regex = /(<head>)([\s\S]*?<\/head>)/i
+            const match = content.match(regex)
+
+            if(match) {
+                content = content.replace(match[0], match[1] + clientScript + match[2])
+            }
+        }
         res.writeHead(200, {
             "Content-Type": contentType
         })
